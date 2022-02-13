5fe3d6cbf74703e3562c5c5748d60f98dabdd773
finer <finersanya@gmail.com>
2022-02-13 19:57:49 +0500

    Staff model

---
+++ app/Admin/Worker.php
@@ -0,0 +1,43 @@
+<?php
+
+use App\Models\Worker;
+
+use SleepingOwl\Admin\Contracts\Display\Extension\FilterInterface;
+use SleepingOwl\Admin\Model\ModelConfiguration;
+
+AdminSection::registerModel(Worker::class, function (ModelConfiguration $model) {
+    $model->setTitle('Персонал');
+
+    $model->onDisplay(function () {
+        $display = AdminDisplay::datatablesAsync();
+
+        $display->setColumnFilters([
+            null,
+
+            AdminColumnFilter::text()
+                ->setPlaceholder('ФИО')
+                ->setOperator(FilterInterface::CONTAINS)
+                ->setHtmlAttribute('style', 'width: 100%'),
+
+        ]);
+        $display->getColumnFilters()->setPlacement('table.header');
+        $display->setColumns([
+            AdminColumn::text('id')->setLabel('#'),
+            AdminColumn::text('name')->setLabel('ФИО'),
+        ]);
+
+        $display->paginate(15);
+        return $display;
+    });
+
+    $model->onCreateAndEdit(function () {
+        $form = AdminForm::panel();
+
+        $form->addBody([
+            AdminFormElement::text('name', 'ФИО')->required(),
+        ]);
+        return $form;
+    });
+})
+    ->addMenuPage(Worker::class, 1)
+    ->setIcon('fa fa-user');
