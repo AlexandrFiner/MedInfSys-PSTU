08ac3493e1df9758e1544c7e7c88541df84fce34
alexandr <finersanya@gmail.com>
2022-04-03 20:27:02 +0500

    Прикрепление рабочего персонала к больницам и поликлиникам

--- app/Admin/Worker.php
+++ app/Admin/Worker.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Hospital;
+use App\Models\Polyclinic;
 use App\Models\ProfileWorkers;
 use App\Models\Worker;
 
@@ -26,12 +28,26 @@ AdminSection::registerModel(Worker::class, function (ModelConfiguration $model) 
                 ->multiple()
                 ->setHtmlAttribute('style', 'width: 100%'),
 
+            AdminColumnFilter::select()
+                ->setModelForOptions(Polyclinic::class, 'name')
+                ->setColumnName('polyclinics.id')
+                ->multiple()
+                ->setHtmlAttribute('style', 'width: 100%'),
+
+            AdminColumnFilter::select()
+                ->setModelForOptions(Hospital::class, 'name')
+                ->setColumnName('hospitals.id')
+                ->multiple()
+                ->setHtmlAttribute('style', 'width: 100%'),
+
         ]);
         $display->getColumnFilters()->setPlacement('table.header');
         $display->setColumns([
             AdminColumn::text('id')->setLabel('#'),
             AdminColumn::text('name')->setLabel('ФИО'),
             AdminColumn::text('profile.name')->setLabel('Должность'),
+            AdminColumn::lists('polyclinics.name', 'Polyclinic')->setLabel('Поликлиники'),
+            AdminColumn::lists('hospitals.name', 'Hospital')->setLabel('Больницы'),
         ]);
 
         $display->paginate(15);
@@ -46,6 +62,8 @@ AdminSection::registerModel(Worker::class, function (ModelConfiguration $model) 
             AdminFormElement::select('profile_workers_id', 'Должность', ProfileWorkers::class)
                 ->setDisplay('name')
                 ->required(),
+            AdminFormElement::multiselect('polyclinics', 'Поликлиники', Polyclinic::class)->setDisplay('name'),
+            AdminFormElement::multiselect('hospitals', 'Больницы', Hospital::class)->setDisplay('name'),
         ]);
         return $form;
     });
